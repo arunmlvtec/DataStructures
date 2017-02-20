e8296acafa27a0bbd0f91fb65a46c9cec92f3179
Arun Pokharna <arun.pokharna@gmail.com>
2017-02-19 16:46:17 -0800

    Initial Checkin of DataStructures project. LinkedList implementation

---
+++ src/linkedlist/Node.java
@@ -0,0 +1,27 @@
+package linkedlist;
+
+public class Node {
+	int num;
+	Node link;
+	public Node(){
+		num = 0;
+		link = null;
+	}
+	public Node(int num, Node link){
+		this.num = num;
+		this.link = link;
+	}
+	public void setNum(int num){
+		this.num = num;
+	}
+	public void setLink(Node link){
+		this.link = link;
+	}
+	public int getNum(){
+		return num;
+	}
+	public Node getLink(){
+		return link;
+	}
+	
+}
